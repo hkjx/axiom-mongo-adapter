8749602471039d7e4588adb8c81d0424ebc0e365
hkjx <hkjxaa@gmail.com>
2013-10-17 15:42:37 +0300

    adding insertion method

--- lib/axiom/tuple.rb
+++ lib/axiom/tuple.rb
@@ -2,8 +2,10 @@ module Axiom
 
   # A set of objects representing a unique fact in a relation
   class Tuple
-    def to_hash
-      Hash[data.map { |attribute, value| [attribute.name, value] }]
+    class_eval do 
+      'def to_hash
+        Hash[data.map { |attribute, value| [attribute.name, value] }]
+      end'
     end
   end
 
